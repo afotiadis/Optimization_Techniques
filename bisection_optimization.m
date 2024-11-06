6f77619cf08d4bb776982c599077f4b2edef7b3b
afotiadis <afotiadis@ece.auth.gr>
2024-11-06 12:08:50 +0200

    Update bisection_optimization.m

--- bisection_optimization.m
+++ bisection_optimization.m
@@ -88,3 +88,36 @@ plot(l,2*(k-1),'-r','LineWidth',1.4)
 title('$f_3(x) = e^{x^3 - 1} + (x-1) \cdot \sin(x)$','Interpreter', 'latex')
 xlabel('l') 
 ylabel('numbers of iterations') 
+
+%=======================================================================
+%Giving l some standard values
+figno=3;
+epsilon=0.001;
+l_val=[0.01,0.05,0.1];
+funcNames = {
+    '$f_1(x) = (x-2)^2 + x \cdot \ln(x+3)$', ...
+    '$f_2(x) = e^{-2x} + (x-2)^2$', ...
+    '$f_3(x) = e^{x^3 - 1} + (x-1) \cdot \sin(x)$'
+};
+
+F={f_1,f_2,f_3}; %struct that contains all the functions 
+
+%In this loop we study how the edges of the search area change during the
+%algorithm's performance
+for j=1:3
+    figure(figno)
+    figno=figno+1;
+    for n=1:3
+        subplot(3,1,n)
+        [a,b,k]=bisectionalgorithm(F(j),epsilon,l_val(n));
+        for i=0:1:k-1
+            plot(i,a(i+1),'*r')
+            hold on;
+            plot(i,b(i+1),"ob")
+        end
+        xlim([0 12])
+        title(funcNames(1,j),'Interpreter','latex')
+        xlabel(sprintf("number of iterations for l=%.2f",l_val(n)))
+        ylabel("[a,b]")
+    end
+end
